295a09285826e81d0b8d3741353f99a26ab7bdc9
JustItDo <diaxodiax@gmail.com>
2024-10-24 09:13:08 +0200

    dodanie form logowania

---
+++ src/components/logForm.jsx
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus tempore ipsum corrupti sed optio ut odit nihil debitis inventore quam sit laudantium quas consequatur asperiores, hic suscipit vitae fugit ipsam?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, dolores.</p>
+<ul>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+    <li>Lorem ipsum dolor sit amet.</li>
+</ul>
+</body>
+</html>
